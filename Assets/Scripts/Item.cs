99d8df1fc6deb29eddd73ced4f7911fab93f9dea
AndSelm <and.selm+git@gmail.com>
2019-08-13 21:23:30 +0200

    initial commit (including auto-generated sln and csproj)

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public int someField; // instance_fields_should_be_camel_case
+
+    public Item(string name) // unused parameter (VS default)
+    {
+        someField = 3; // dotnet_style_qualification_for_field
+    }
+
+    static public Item CreateItem()
+    {
+        return null; // csharp_style_expression_bodied_methods
+    }
+}
